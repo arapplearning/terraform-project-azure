100df3765969c7e6aa63218ebaced2515f6da8ac
arapplearning <arapplearning@gmail>
2025-01-23 12:02:42 +1300

    Added env variables

--- main.tf
+++ main.tf
@@ -10,7 +10,10 @@ terraform{
 
 provider "azurerm" {
   features {}
-
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 #Added resource group for dev
